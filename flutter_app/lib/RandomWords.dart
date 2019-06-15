7da54ce6d7666866148b32bf065351fa028e20ce
ridzhang619 <ridzhang619@126.com>
2019-06-15 16:00:40 +0800

    测试listview

---
+++ flutter_app/lib/RandomWords.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/RandomWordsState.dart';
+
+class RandomWords extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordsState();
+  }
+
+}
+
---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter_app/RandomWords.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+    return new MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('Welcome to Flutter'),
+//        ),
+//        body: new Center(
+////          child: new Text('Hello World'),
+////            child: new Text(wordPair.asPascalCase),
+//        child: new RandomWords(),
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
+//        ),
+//      ),
+    );
+  }
+}
